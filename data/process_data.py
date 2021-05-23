1d0668de0054eb7b8654d1ad48c95d5bab9abcf2
Felix.Pohl <pohl.felix89@web.de>
2021-05-23 20:30:12 +0200

    implemented load step of ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,47 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+	'''reads provided csv files and returns a dataframe merged on the id column'''
+	# load messages dataset
+	messages = pd.read_csv(messages_filepath)
+	# load categories dataset
+	categories = pd.read_csv(categories_filepath)
+	# merge datasets and return
+	return messages.merge(categories, on='id')
 
 
 def clean_data(df):
-    pass
+	'''create one-hot-encoding of categories column and removes duplicates in the messages column'''
+	# create a dataframe of the individual category columns
+	categories = df['categories'].str.split(';', expand=True)
+	# select the first row of the categories dataframe
+	row = df['categories'].str.split(';', expand=True).iloc[0]
+	# convert values into category names by removing number and '-'
+	category_colnames = row.str.replace('-\d','')
+	# rename the columns of `categories`
+	categories.columns = category_colnames
+	# convert category values to binary encoding (0,1)
+	for column in categories:
+		# set each value to be the last character of the string
+		categories[column] = categories[column].astype(str).str[-1]
+		
+		# convert column from string to numeric
+		categories[column] = pd.to_numeric(categories[column])
+	# remove unnecessary categories column
+	df.drop('categories', axis=1, inplace=True, errors='ignore')
+	# concat categories as one-hot-encoding and message data
+	df = pd.concat([df, categories], axis=1)
+	# remove duplicates by 'message' column
+	df = df.drop_duplicates(subset=['message'])
+	return df
 
 
 def save_data(df, database_filename):
-    pass  
+	'''save df into provided database in table 'messages' '''
+	engine = create_engine('sqlite:///{}'.format(database_filename))
+	df.to_sql('messages', engine, index=False)
 
 
 def main():
